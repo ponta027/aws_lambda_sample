2b5ffb8428b9f6e0f7eacfa5cf823654a782f2ac
ponta027 <ponta027@gmail.com>
2020-06-21 02:12:58 +0900

    move

--- src/sample.ts
+++
@@ -1,9 +0,0 @@
-export function f1(){
-        //        console.log("TEST");
-        //
-    return {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda! CLI'),
-    };
-
-}
